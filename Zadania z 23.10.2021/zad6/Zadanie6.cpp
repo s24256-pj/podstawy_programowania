b7e06e541c2190094bf48cc45aacec42983f21da
Marta Szpilka <s24256@pjwstk.edu.pl>
2021-11-05 19:08:47 +0100

    Dodanie szostego zadania

---
+++ Zadania z 23.10.2021/zad6/Zadanie6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    int i=1;
+    double wynik = 1;
+
+    cout << "Podaj podstawe potegi: " << endl;
+    cin >> x;
+    cout << "Podaj wykladnik potegi: " << endl;
+    cin >> y;
+
+    do
+    {
+        wynik = wynik*x;
+        i = i+1;
+    }
+    while(i<=y);
+
+    cout << wynik << endl;
+
+    return 0;
+}
